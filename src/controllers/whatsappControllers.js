01fdc3fb5fe10ac0141f9787463d756bd4ed6fd8
Alejandro <alesercad@gmail.com>
2024-03-03 18:17:02 -0400

    la configuracion del token en el webhook de facebok listas y desplegradas en azure

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -2,9 +2,9 @@ const verifytoken = (req, res) => {
     try {
         var accessToken = "RTQCADENA12332" //creamos un token para configurar en el webhook dela app de varfacebook 
         var token = req.query["hub.verify_token"]; //capturamos lo que manda facebook en la documentacion esta asi
-        var challengue = req.body["hub.challengue"]; //es para comparar el token que nos envia es el mismo que nosotros tenemos 
-        if (challengue != null && token != null && accessToken == token) { //condicionamos que os dos sean diferntes de null y sean iguales
-            res.send(challengue); //si todo es correcto devolvemos el challengue
+        var challenge = req.query["hub.challenge"]; //es para comparar el token que nos envia es el mismo que nosotros tenemos 
+        if (challenge != null && token != null && accessToken == token) { //condicionamos que os dos sean diferntes de null y sean iguales
+            res.send(challenge); //si todo es correcto devolvemos el challengue
         } else {
             res.status(400).send();
         }
@@ -12,14 +12,14 @@ const verifytoken = (req, res) => {
         res.status(400).send(); //en la documentacion dice que si da error ttiene que dar 400
     }
 
-};
+}; //se usa solo en el token
 
 
 
 
 const recibeMessage = (req, res) => {
     res.send("HOla Recived");
-};
+}; //se usa cada vez que recibimios un mensaje
 
 //Exportamos lo metodos
 module.exports = {
